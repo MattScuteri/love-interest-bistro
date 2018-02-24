1b00ae48c3c09f637f8a7c93ce7c3f3aea26193a
MattScuteri <mttscuteri@gmail.com>
2018-02-24 11:40:04 -0500

    module export added

--- ajax.js
+++ ajax.js
@@ -5,27 +5,28 @@ const path = require("path");
 const app = express();
 const PORT = 3000;
 
-app.get("/", function(req, res) {
-	res.sendFile(path.join(__dirname, "home.html"))
-});
+module.exports: function(ajax) {
+	app.get("/", function(req, res) {
+		res.sendFile(path.join(__dirname, "home.html"))
+	});
 
-app.get("/reservation", function(req, res) {
-	res.sendFile(path.join(__dirname, "reserve.html"))
-});
+	app.get("/reservation", function(req, res) {
+		res.sendFile(path.join(__dirname, "reserve.html"))
+	});
 
-app.get("/tables", function(req, res) {
-	res.sendFile(path.join(__dirname, "tables.html"))
-})
+	app.get("/tables", function(req, res) {
+		res.sendFile(path.join(__dirname, "tables.html"))
+	})
 
-app.post("/api/new", function(req, res) {
-	const newReservation = req.body;
+	app.post("/api/new", function(req, res) {
+		const newReservation = req.body;
 
-	newReservation.routeName = newReservation.name.replace(/\s+/g, "").toLowerCase()
+		newReservation.routeName = newReservation.name.replace(/\s+/g, "").toLowerCase()
 
-	console.log(newReservation);
+		console.log(newReservation);
 
-	reservation.push(newReservation);
+		reservation.push(newReservation);
 
-	res.json(newReservation);
-})
-
+		res.json(newReservation);
+	})
+}
